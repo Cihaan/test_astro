e38b48648f18e477e70b29df772fd2ef0eec218f
Cihaan <cihankafadar1@gmail.com>
2023-01-13 20:46:30 +0100

    feat: about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="h-screen w-screen">
-        <img class="w-20" src="../../public/gyga.jpg" alt="gyga chad" />
-        <h2>Cihan KAFADAR</h2>
-
+        <article class="flex flex-col w-full gap-4 items-center mt-5">
+            <img class="w-[200px] rounded-2xl" src="../../public/gyga.jpg" alt="gyga chad" />
+            <div class="flex flex-col items-center w-full">
+                <h2 class="text-2xl">Cihan KAFADAR</h2>
+                <p>Full Stack developer doing code stuff</p>
+            </div>
+        </article>
     </div>
 </template>
